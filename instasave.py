d0413649c26a27410c2ddd9bb23e950b0e5f63b1
Shubham Sarda <shubhamsarda99@yahoo.com>
2019-06-10 13:07:53 +0530

    Insta Save File

---
+++ instasave.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import requests
+from bs4 import BeautifulSoup
+
+app= Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+	if request.method=='POST' and 'iurl' in request.form: 
+		url=request.form['iurl']
+		raw=requests.get(url)
+		soup=BeautifulSoup(raw.text,'html.parser')
+		links= soup.find(property="og:image")
+		image=links.get('content')
+		while image!='':
+			return '<img src="'+image+'"'+ 'align="center">'
+	return render_template('index.html')
+
+if __name__=='__main__':
+	app.run()
